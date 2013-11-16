d25712093cfd5e220e5b138b6f2771b46b2dc08a
LiwenWen <lwen@giant-linux.(none)>
2013-11-15 17:08:47 -0800

    add some cuts functions

--- lambdaXiPair.C
+++ lambdaXiPair.C
@@ -2,12 +2,17 @@
 #include "TFile.h"
 #include "TH1F.h"
 #include "TChain.h"
+#include "StV0dst.h"
 void lambdaXiPair(){
+//================Initialize Some Parameters================================================
+    Double_t xiMass = 1.321;
+    Dobule_t lambdaMass = 1.115;
+    
 //================Chain Them And Initialize The Accepting Structure==========================
     TChain* chain = new TChain("chainTree");
     chain -> Add("/media/qi_/day133/*picodst*");
     
-    gRoot -> LoadMacro("v0dst.C");
+    gRoot -> LoadMacro("v0dst.C");// Just load this structure. but we can do by including header file, right? But this is just a macro. Anyway we have to load the StPhysical Helix header file
     v0dst v0dst(chain);
 
 //================Book Histograms===========================================
@@ -18,6 +23,7 @@ void lambdaXiPair(){
 
 //================Apply Cuts To Pair Lambda And Cascade Minus====================
     for(Int_t i = 0; i < nEvent; i++){
+
         Int_t currentEntry  = v0dst -> LoadTree(i); 
         if(currentEntry < 0) continue;
 
@@ -26,12 +32,14 @@ void lambdaXiPair(){
 
         for(Int_t j = 0; j < numberOfLambda; j++){
 
-            Double_t xiMass = v0dst.v0mass_xi;
             StPhysicalHelix lambdaHelix//TODO
+          
+            if(!pSetOfCutsLambda(v0dst)) continue;//TODO: Implement this function
 
             for(Int_t k = 0; k < numberOfXi; k++){
                 
-                Double_t lambdaMass = v0dst.v0mass_Lambda;
+                if(!pSetOfCutsXi(v0dst)) continue;//TODO: Implement this cuts set function
+
                 StPhysicalHelix xiHelix//TODO
                 StThreeVectorF boundStatePosition;
                 StThreeVectorF lambdaP, xiP;
@@ -54,10 +62,36 @@ void lambdaXiPair(){
         }
     }
 
-//================Fill Histograms============================
+//================Fill Histograms=======================================
 }
 
 Bool_t pCutLambdaXiDca(Double_t dca){
     if(dca > dcaLeEq) return false;
+    else return true;
+}
+
+Bool_t pSetOfCutsLambda(v0dst v0dst){// TODO: finish these blanks
+    if(v0dst.v02PV > ) return false;
+    if(v0dst.v0Rapidity > ) return false;
+    if(v0dst.v0DecLength > ) return false;
+    if(v0dst.v0Dca2Dau >) return false;
+    if(v0dst.dau1Dca < ) return false;
+    if(v0dst.dau2Dca < ) return false;
+    if(v0dst.dau1NsigmaProton > ) return false;
+    if(v0dst.dau2NsigmaPion > ) return false;
+   
+    else return true;
+}
+
+Bool_t pSetOfCutsxi(v0dst v0dst){// TODO: Double check whether this is ok for Xi or we need some modification 
+    if(v0dst.v02PV > ) return false;
+    if(v0dst.v0Rapidity > ) return false;
+    if(v0dst.v0DecLength > ) return false;
+    if(v0dst.v0Dca2Dau >) return false;
+    if(v0dst.dau1Dca < ) return false;
+    if(v0dst.dau2Dca < ) return false;
+    if(v0dst.dau1NsigmaProton > ) return false;
+    if(v0dst.dau2NsigmaPion > ) return false;
+   
     else return true;
 }
